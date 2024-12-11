d2fd8f5407821783b4ba1c99aadc2c05e7c89924
ANAS <anaspappadan@gmail.com>
2024-12-11 12:06:43 +0530

    initialize project with TypeScript, Express, and ESLint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["node_modules", "build"] },
+  { files: ["src/**/*.ts"] },
+  { languageOptions: { globals: globals.node } },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
